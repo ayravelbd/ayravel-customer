e7bfc2c12349f13a664b30fcd60f86905f7b9759
ridoy-softworldit <Ridoy.softworldit@gamil.com>
2025-10-30 03:32:29 +0600

    ayravel customer done

---
+++ src/components/modules/Navbar/NavbarLogo.tsx
@@ -0,0 +1,31 @@
+"use client";
+import React from "react";
+import MobileMenu from "./MobileMenu";
+import Link from "next/link";
+import Image from "next/image";
+import { useGetSettingsQuery } from "@/redux/featured/settings/settingsApi";
+
+const NavbarLogo = () => {
+  const { data: settings } = useGetSettingsQuery();
+  
+  return (
+    <div>
+      <MobileMenu />
+      <Link href="/" className="flex items-center">
+        {settings?.logo ? (
+          <Image
+            src={settings.logo}
+            alt="AYravel Logo"
+            width={120}
+            height={40}
+            className="h-8 w-auto"
+          />
+        ) : (
+          <span className="text-xl font-bold">AYravel</span>
+        )}
+      </Link>
+    </div>
+  );
+};
+
+export default NavbarLogo;
